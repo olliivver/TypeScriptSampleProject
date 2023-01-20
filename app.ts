e2de55d8491ac6f0e6a6cb88e7a16b2c983c5c29
Oliver Mercer-Smail <oliver.finlay@gmail.com>
2023-01-20 09:56:27 -0500

    enums

--- app.ts
+++ app.ts
@@ -3,18 +3,30 @@ const num2 = document.getElementById("num2");
 const button = document.getElementById("button");
 const output = document.getElementById("output");
 
-const human: {
-  name: string;
-  nickname: string;
-  age: number;
-  occupation: string;
-  learning: string;
-} = {
+button?.addEventListener("click", () => {});
+enum Role {
+  ADMIN,
+  USER,
+  READ_ONLY,
+}
+const human = {
+  //   name: string;
+  //   nickname: string;
+  //   age: number;
+  //   occupation: string;
+  //   learning: string;
+  //   hobbies: string[];
+  // } =
+  //{
   name: "oliver",
   age: 30,
   nickname: "oli",
   occupation: "coder",
-  learning:"typescript"
+  learning: "typescript",
+  hobbies: ["jiu jitsu", "piano"],
+  role: Role.ADMIN,
 };
+//tuple - array with 2 elements, which you tell tsc what they are
+//enum - enumerated list, assigns labels to numbers
 
-console.log(human.nickname);
+console.log(human.nickname), human.role;
